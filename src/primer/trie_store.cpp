482980ab9b3e57d86896f3b65dcc8e5e944088da
nelloloujh <nelloloujh@gmail.com>
2024-03-28 20:21:12 +0800

    P0 Task2 Finish,Pass Trie_store_test.cpp

--- src/primer/trie_store.cpp
+++ src/primer/trie_store.cpp
@@ -11,20 +11,58 @@ auto TrieStore::Get(std::string_view key) -> std::optional<ValueGuard<T>> {
   // (2) Lookup the value in the trie.
   // (3) If the value is found, return a ValueGuard object that holds a reference to the value and the
   //     root. Otherwise, return std::nullopt.
-  throw NotImplementedException("TrieStore::Get is not implemented.");
+  //throw NotImplementedException("TrieStore::Get is not implemented.");
+
+
+  std::shared_ptr<const TrieNode> root_copy;
+  //给上一个锁
+  std::lock_guard<std::mutex> lock(root_lock_);
+
+  //上锁完之后可以开始复制并操作了
+  auto trie =Trie();
+  trie=root_;
+
+  const T* value = trie.Get<T>(key);
+
+  //题目要求封装返回
+  if(value){
+    return ValueGuard(trie,*value);
+  }else{
+    return std::nullopt;
+  }
+
 }
 
 template <class T>
 void TrieStore::Put(std::string_view key, T value) {
   // You will need to ensure there is only one writer at a time. Think of how you can achieve this.
   // The logic should be somehow similar to `TrieStore::Get`.
-  throw NotImplementedException("TrieStore::Put is not implemented.");
+//  throw NotImplementedException("TrieStore::Put is not implemented.");
+
+  //自动解锁用lock_guard
+  std::lock_guard<std::mutex> lock(write_lock_);
+  auto trie =Trie();
+  trie=root_;
+
+  trie=trie.Put(key,std::move(value));
+
+  std::lock_guard<std::mutex> root_lock(root_lock_);
+  root_ = trie;
+//  const T* value = trie.Get<T>(key);
+
 }
 
 void TrieStore::Remove(std::string_view key) {
   // You will need to ensure there is only one writer at a time. Think of how you can achieve this.
   // The logic should be somehow similar to `TrieStore::Get`.
-  throw NotImplementedException("TrieStore::Remove is not implemented.");
+//  throw NotImplementedException("TrieStore::Remove is not implemented.");
+  std::lock_guard<std::mutex> lock(write_lock_);
+  auto trie =Trie();
+  trie=root_.Remove(key);
+
+
+  std::lock_guard<std::mutex> root_lock(root_lock_);
+  root_ = trie;
 }
 
 // Below are explicit instantiation of template functions.

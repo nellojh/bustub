de7490052a102d0183026c1b9d647881fd748a19
nelloloujh <nelloloujh@gmail.com>
2024-06-09 17:30:19 +0800

    6.9 Project2 1/3

--- src/storage/page/page_guard.cpp
+++ src/storage/page/page_guard.cpp
@@ -2,16 +2,70 @@
 #include "buffer/buffer_pool_manager.h"
 
 namespace bustub {
+/** TODO(P1): Add implementation
+ *
+ * @brief Move constructor for BasicPageGuard
+ *
+ * When you call BasicPageGuard(std::move(other_guard)), you
+ * expect that the new guard will behave exactly like the other
+ * one. In addition, the old page guard should not be usable. For
+ * example, it should not be possible to call .Drop() on both page
+ * guards and have the pin count decrease by 2.
+ */
 
-BasicPageGuard::BasicPageGuard(BasicPageGuard &&that) noexcept {}
+BasicPageGuard::BasicPageGuard(BasicPageGuard &&that) noexcept {
+  bpm_ = that.bpm_;
+  is_dirty_ = that.is_dirty_;
+  page_ = that.page_;
 
-void BasicPageGuard::Drop() {}
+  // delete that
+  that.page_ = nullptr;
+  that.bpm_ = nullptr;
+}
 
-auto BasicPageGuard::operator=(BasicPageGuard &&that) noexcept -> BasicPageGuard & { return *this; }
+void BasicPageGuard::Drop() {
+  if (bpm_ != nullptr && page_ != nullptr) {
+    bpm_->UnpinPage(page_->GetPageId(), is_dirty_);
+  }
 
-BasicPageGuard::~BasicPageGuard(){};  // NOLINT
+  page_ = nullptr;
+  bpm_ = nullptr;
+}
 
-auto BasicPageGuard::UpgradeRead() -> ReadPageGuard { return {bpm_, page_}; }
+auto BasicPageGuard::operator=(BasicPageGuard &&that) noexcept -> BasicPageGuard & {
+  Drop();
+  bpm_ = that.bpm_;
+  page_ = that.page_;
+  // delete that
+  that.page_ = nullptr;
+  that.bpm_ = nullptr;
+
+  this->is_dirty_ = that.is_dirty_;
+
+  return *this;
+}
+
+BasicPageGuard::~BasicPageGuard() { Drop(); };  // NOLINT
+/** TODO(P1): Add implementation
+ *
+ * @brief Upgrade a BasicPageGuard to a ReadPageGuard
+ *
+ * The protected page is not evicted from the buffer pool during the upgrade,
+ * and the basic page guard should be made invalid after calling this function.
+ *
+ * @return an upgraded ReadPageGuard
+ */
+
+auto BasicPageGuard::UpgradeRead() -> ReadPageGuard {
+  if (page_ != nullptr) {
+    page_->RLatch();
+  }
+  auto read_page_guard = ReadPageGuard(bpm_, page_);
+  bpm_ = nullptr;
+  page_ = nullptr;
+
+  return read_page_guard;
+}
 
 auto BasicPageGuard::UpgradeWrite() -> WritePageGuard { return {bpm_, page_}; }
 
